3252a2dac24b9b10d5194302764f2fef687e10a8
griswoldrx <griswoldrx@gmail.com>
2017-02-08 22:53:45 -0500

    Completed Project2.py

--- proj2.py
+++ proj2.py
@@ -1,29 +1,78 @@
-#proj2.py
-
+import requests
+from bs4 import BeautifulSoup
+import re
 
 #### Problem 1 ####
 print('\n*********** PROBLEM 1 ***********')
 print('New York Times -- First 10 Story Headings\n')
+url = 'http://nytimes.com'
+result = requests.get(url)
+html = result.text
+soup = BeautifulSoup(html, 'html.parser')
 
-### Your Problem 1 solution goes here
+story_heading = soup.find_all('h2', class_ = 'story-heading')
+for i in range(10):
+	if story_heading[i].a:
+		print(story_heading[i].a.text.replace('\n', " ").strip())
+	else:
+		print(story_heading[i].contents[0].strip())
 
-
-#### Problem 2 ####
+# #### Problem 2 ####
 print('\n*********** PROBLEM 2 ***********')
 print('Michigan Daily -- MOST READ\n')
 
-### Your Problem 2 solution goes here
+mi_url = 'https://www.michigandaily.com/'
+mi_result = requests.get(mi_url)
+mi_html = mi_result.text
+mi_soup = BeautifulSoup(mi_html, 'html.parser')
 
+for div in mi_soup.find_all('div', {'class':"panel-pane pane-mostread"}):
+	for li in div.find_all('li'):
+		print(li.a.contents[0])
 
-#### Problem 3 ####
+# #### Problem 3 ####
 print('\n*********** PROBLEM 3 ***********')
 print("Mark's page -- Alt tags\n")
 
-### Your Problem 3 solution goes here
+marks_url = "http://newmantaylor.com/gallery.html"
+marks_result = requests.get(marks_url)
+marks_html = marks_result.text
+marks_soup = BeautifulSoup(marks_html, 'html.parser')
 
+for img in marks_soup.find_all('img'):
+	try:
+		print(img['alt'])
+	except:
+		print("No alternative text provided!!")
 
 #### Problem 4 ####
 print('\n*********** PROBLEM 4 ***********')
 print("UMSI faculty directory emails\n")
 
-### Your Problem 4 solution goes here
+def getemails(url, chunk):
+	count = 1
+	umsi_result = requests.get(umsi_url)
+	umsi_html = umsi_result.text
+	umsi_soup = BeautifulSoup(umsi_html, 'html.parser')
+	for div in umsi_soup.find_all('div', {'class' : "field-item even"}):
+		for a in div.find_all('a', href = True):
+			link = a['href']
+			new_page = requests.get('https://www.si.umich.edu' + link)
+			new_page_html = new_page.text
+			new_page_soup = BeautifulSoup(new_page_html, 'html.parser')
+			for div in new_page_soup.find_all('div', {'class' : "field-item even"}):
+				for a1 in div.find_all('a', href= True):
+					email = a1['href']
+					x = re.findall('mailto:(.+)', email)
+					if x != []:
+						print(count + chunk, x[0])
+						count+=1
+
+chunk = 0
+page = 1
+umsi_url = "https://www.si.umich.edu/directory?field_person_firstname_value=&field_person_lastname_value=&rid=4"			
+for each in range(6):
+	getemails(umsi_url, chunk)
+	umsi_url = umsi_url + '&page=' + str(page)
+	chunk = chunk +20
+	page +=1
